c553995cd95eeb9375f13121882bd0943db249a8
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-04-16 11:26:29 +0530

    created server using http

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs=require('fs');
+/*const fs=require('fs');
 let textIn=fs.readFileSync('./files/files.txt','utf-8');
 fs.readFile('./Files/start.txt','utf-8',(error1,data1)=>{
     console.log(data1)
@@ -6,4 +6,20 @@ fs.readFile('./Files/start.txt','utf-8',(error1,data1)=>{
         console.log(data2);
     })
 })
-console.log(textIn)
+console.log(textIn)*/
+
+//how we are going to create a http server
+//step1.CREATE THE SERVER
+//step2.Start the SERVER
+
+const http=require('http');
+const server=http.createServer(
+    (request,response)=>{
+        response.end('hello from the server!');
+        console.log('a new request recieved');
+
+    }
+);
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("server has started!");
+})
