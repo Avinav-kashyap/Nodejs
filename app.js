bec05f3ad6a271f1b5bf0f92dce4effe4dce0a17
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-04-16 10:26:45 +0530

    files io done asynchronously

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const fs=require('fs');
 let textIn=fs.readFileSync('./files/files.txt','utf-8');
+fs.readFile('./Files/start.txt','utf-8',(error1,data1)=>{
+    console.log(data1)
+    fs.readFile(`./Files/${data1}.txt`,'utf-8',(error2,data2)=>{
+        console.log(data2);
+    })
+})
 console.log(textIn)
