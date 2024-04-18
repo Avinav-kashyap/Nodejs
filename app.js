d9230052a57aacdc76ed5f5ccca99ef037df97d8
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-04-18 11:30:05 +0530

    done with routing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ console.log(textIn)*/
 //step1.CREATE THE SERVER
 //step2.Start the SERVER
 
-const http=require('http');
+/**const http=require('http');
 const server=http.createServer(
     (request,response)=>{
         response.end('hello from the server!');
@@ -22,4 +22,50 @@ const server=http.createServer(
 );
 server.listen(8000,'127.0.0.1',()=>{
     console.log("server is going to be started");
-})
+})**/
+
+//lec 14
+const fs=require('fs');
+const http=require('http');
+const html=fs.readFileSync('./Template/index.html','utf-8')
+
+const server=http.createServer((request,response)=>{
+    let path=request.url;
+    if(path === '/' || path.toLocaleLowerCase() ==='/home')
+    {
+        response.writeHead(200,{
+            'Content-type':'text/html',
+            'my-header':'Hello world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','You are in Home Page now'));
+    }
+    else if(path.toLocaleLowerCase() === '/about')
+    {
+        response.writeHead(200,{
+            'Content-type':'text/html',
+            'my-header':'Hello world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','YOU ARE IN ABOUT page now'));
+    }
+    else if(path.toLocaleLowerCase()=== '/contact')
+    {
+        response.writeHead(200,{
+            'Content-type':'text/html',
+            'my-header':'Hello world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','You are in contact page now'));
+    }else{
+        response.writeHead(404,{
+            'Content-type':'text/html',
+            'my-header':'Hello world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','Error 404:page not found!'));
+    }
+    
+
+
+});
+server.listen(8000,'127.0.0.1',()=>{
+      console.log('server has started');
+
+});
