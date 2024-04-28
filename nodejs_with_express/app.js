38e7911eb194381bd5f8893d62c66732412a02f3
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-04-28 17:11:12 +0530

    HANDLES POST REQUEST

--- nodejs_with_express/app.js
+++ nodejs_with_express/app.js
@@ -1,14 +1,44 @@
 const express=require('express');
+const fs=require('fs');
+
+
 let app=express();
+let movies=JSON.parse(fs.readFileSync('./data/movies.json'));
+
+app.use(express.json());
 
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'hello ,world',status:200});
-    
-})
+//GET -  api/v1/movies
+app.get('/api/v1/movies',(req,res)=>{
+    res.status(200).json({
+        status: "success",
+        count:movies.length,
+        data:{
+             movies:movies
 
-app.post('/',()=>{
-    
+        }
+    });
+
+});
+
+
+//POST-api/v1/movies
+app.post('/api/v1/movies',(req,res)=>{
+   // console.log(req.body);
+    const newId=movies[movies.length-1].id +1;
+    const newMovie=Object.assign({id:newId},req.body)
+    movies.push(newMovie);
+
+    fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
+             res.status(201).json({
+                status:"success",
+                data:{
+                    movie:newMovie
+                }
+             })
+    })
+   // res.send('Created');
+
 })
 
 //create a server
