966528bbdae3a73df4a5de42a5c2726a35797170
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-04-29 11:02:16 +0530

    HANDLED PATCH REQUEST

--- nodejs_with_express/app.js
+++ nodejs_with_express/app.js
@@ -66,6 +66,33 @@ app.post('/api/v1/movies',(req,res)=>{
 
 })
 
+app.patch('/api/v1/movies/:id',(req,res)=>{
+    let id=req.params.id * 1;
+    let movieToUpdate=movies.find(el => el.id === id);
+
+    if(!movieToUpdate){
+        return res.status(404).json({
+            status:'fail',
+            message :"No movie object with ID"+ id +"is found"
+        })
+
+    }
+    let index=movies.indexOf(movieToUpdate);
+
+    Object.assign(movieToUpdate,req.body);
+    movies[index]=movieToUpdate;
+
+    fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
+        res.status(200).json({
+            status:"success",
+            data:{
+                movie:movieToUpdate
+            }
+         })
+
+
+    })
+})
 //create a server
 const port=3000;
 app.listen(port,()=>{
