30ce22ae88e74bc970bdc7e6b3cecf268a287986
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-05-01 23:21:58 +0530

    Connected to remote DATABASE

--- nodejs_with_express/server.js
+++ nodejs_with_express/server.js
@@ -1,11 +1,24 @@
 //console.log(app.get('env'));
 
 
-
+const mongoose= require('mongoose');
 const dotenv= require('dotenv');
 
 dotenv.config({path :'./config.env'});
 console.log(process.env);
+
+
 const app=require('./app');
+//connection with mongoose
+mongoose.connect(process.env.CONN_STR,{
+    useNewUrlParser: true
+}).then((conn)=>{
+     console.log(conn);
+     console.log('DB Connection Successful');
+
+}).catch((error)=>{
+    console.log('Some error has occured');
+
+});
 
 const port=process.env.PORT;
